a98d923f83095f5736751e8602496239aef80797
Dzianis Buyakou <denis.da.by@gmail.com>
2023-11-22 02:00:31 +0100

    feat: Update resource name of bucket

--- examples/s3/main.tf
+++ examples/s3/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my-tf-test-bucket-sdfsadf" {
-  bucket = "my-tf-test-bucket-sdfsadf"
+resource "random_string" "random" {
+  length = 8
+  special = false
+  upper = false
+  number = false
+}
+
+resource "aws_s3_bucket" "my-tf-test-bucket" {
+  bucket = "my-tf-test-bucket-${random_string.random.result}"
+  lifecycle {
+   prevent_destroy = true
+ }
 }
